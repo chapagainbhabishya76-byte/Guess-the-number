c32e9185631c504ad591b47a82910f3f4b508bb5
chapagainbhabishya76-byte <chapagain.bhabishya76@gmail.com>
2026-09-09 14:17:53 +0545

    Completed

---
+++ .py
@@ -0,0 +1,31 @@
+print("")
+print("==============GUESS THE NUMBER==============")
+print("---> 1 to 100")
+import random
+num = random.randint(1,100)
+print(num)
+while True:
+    guess = input("Guess the number or [Q] to Quit :")
+    if(guess == "Q"):
+        break
+    else:
+        guess = int(guess)
+
+    if(guess>num):
+        if((num - 3)<guess and (num + 3)>guess):
+            print("You are close, try guessing little less then this..\n ")
+        else:
+            print("You are so wrong dude!,try guessing less then this..\n ")
+
+    elif(guess<num):
+            if((num - 3)<guess and (num + 3)>guess):
+                print("You are close, try guessing little high then this..\n ")
+            else:
+                print("You are so wrong dude!,try guessing high then this..\n ")
+
+    elif(guess == num):
+        print("Congratulations, you guessed the right number! ")
+        break
+print("")
+print("---------Game completed sucessfully----------")
+    
